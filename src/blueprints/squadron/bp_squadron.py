81a091b4a6716fd97d47e6a037411483c3b2f2ab
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2024-09-09 21:37:12 -0400

    adds extra scoffolding to blueprints

--- src/blueprints/squadron/bp_squadron.py
+++ src/blueprints/squadron/bp_squadron.py
@@ -1,3 +1,7 @@
 """
 Squadron analytics and other organizational information
 """
+
+from flask import Blueprint
+
+bp = Blueprint("bp_squadron", __name__, template_folder="templates")
--- src/blueprints/student/bp_student.py
+++ src/blueprints/student/bp_student.py
@@ -1,3 +1,7 @@
 """
 Student specific views
 """
+
+from flask import Blueprint
+
+bp = Blueprint("bp_student", __name__, template_folder="templates")
