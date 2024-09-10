014fc531616aaa39db7c2aae9c65ac9c4cbadf4d
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2024-09-10 09:40:39 -0400

    basic setup of the database manager

--- src/database_manager.py
+++ src/database_manager.py
@@ -1,3 +1,28 @@
 """
 Manages all database interactions
 """
+
+from typing import Callable
+from sqlalchemy import create_engine
+
+
+class DatabaseManager:
+    """
+    Handles database interactions
+    """
+
+    database_route = ""
+    engine = create_engine(database_route)
+
+    @classmethod
+    def with_engine(cls, callback: Callable):
+        """exectues callback with the class-based db engine
+
+        Keyword arguments:
+        cls -- class obj
+        callback -- callable
+        Return: None
+        """
+
+        with cls.engine.connect() as connection:
+            callback(connection)
