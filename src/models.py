16d2c60c2816d803e2db9c5dbd69c83a5603d665
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2024-09-17 21:40:53 -0400

    adds model definitions

--- src/models.py
+++ src/models.py
@@ -1,3 +1,49 @@
 """
 Database models for the v341 application
 """
+
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Base(DeclarativeBase):
+    """
+    Base model class
+    """
+
+    # TODO: implement id, date created, last updated fields
+
+
+class Phase(Base):
+    pass
+
+
+class Student(Base):
+    pass
+
+
+class Role(Base):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column("id", Integer, primary_key=True)
+
+    last_name = Column("last_name", String)
+    first_name = Column("first_name", String)
+    middle_initial = Column("middle_initial", String)
+
+    grade = Column("grade", String)
+
+    role_id = Column("role_id", ForeignKey)
