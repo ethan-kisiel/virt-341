aab807c95e948ba6213c00d4466ca37399dbe5e5
“johnnschell95” <johnnschell95@gmail.com>
2024-09-18 13:14:26 -0600

    Worked on students html and js for inputing account information.

--- src/blueprints/student/bp_student.py
+++ src/blueprints/student/bp_student.py
@@ -42,13 +42,13 @@ def profile(student_id: int):
 
     return render_template("profile.html", student_id = student_id)
 
-@student_bp.route("/<int:student_id>/discrepancy")
-def discrepancy(student_id: int):
-    """index endpoint (for testing purposes)
+@student_bp.route("/<int:student_id>/reports")
+def reports(student_id: int):
+    """Endpoint for student reports
 
     Keyword arguments:
-
+    student_id -- primary key of the user object which is a student
     Return: Template
     """
 
-    return render_template("discrepancy.html", student_id = student_id)
+    return render_template("reports.html", student_id = student_id)
