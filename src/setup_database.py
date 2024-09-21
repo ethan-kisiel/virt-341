4ef1e39d7ec56fa272ff4bbe9fb1bd2b66272d5e
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2024-09-21 10:55:53 -0400

    adds database setup file

---
+++ src/setup_database.py
@@ -0,0 +1,15 @@
+"""
+Sets up the database to have necessary data for demonstrating the website
+"""
+
+
+def main():
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    ## CREATE ROLES
+    ## CREATE 
